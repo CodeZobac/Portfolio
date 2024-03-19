f50fc4ce235a7414380cc2a105b5e50bdd15ac89
Afonso Caboz <afonso.caboz@gmail.com>
2024-03-19 21:24:11 +0000

    Updated dynamic website

---
+++ Websites/Portfolio Website/README.md
@@ -0,0 +1,37 @@
+### Hi there 👋
+
+## You can visit my website here: [Codezobac](https://codezobac.github.io/WebsiteCodeZobac/)
+
+# Caboz Portfolio
+
+Welcome to the Caboz Portfolio project! This portfolio showcases my web development journey, featuring a selection of projects that highlight technical skills and creativity.
+
+## Features
+
+- **Dynamic Gallery**: Utilizes JavaScript to dynamically display projects in a carousel.
+- **Responsive Design**: Crafted with mobile-first design principles for an optimal viewing experience across various devices.
+- **Interactive UI**: Engage with the portfolio through interactive elements, enhancing user experience.
+
+## Technologies Used
+
+- HTML5 & CSS3 for structuring and styling.
+- Vanilla JavaScript for dynamic content and interactivity.
+- Docker for easy deployment and environment consistency.
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes.
+
+### Prerequisites
+
+- [Docker](https://www.docker.com/get-started)
+- [Node.js](https://nodejs.org/en/)
+
+### Installation
+
+1. Clone the repository:
+
+```bash
+git clone https://github.com/CodeZobac/CodeZobac.git
+
+```
--- Websites/Portfolio Website/main.js
+++ Websites/Portfolio Website/main.js
@@ -1,42 +1,84 @@
-var swiper = new Swiper(".swiper", {
-  effect: "coverflow",
-  grabCursor: true,
-  centeredSlides: true,
-  slidesPerView: 2,
-  speed: 600,
-  coverflowEffect: {
-    rotate: 0,
-    stretch: 0,
-    depth: 100,
-    modifier: 3,
-    slideShadows: true,
-  },
-  loop: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: false,
-  },
-});
+import Gallery from "./Gallery.js";
+// Path: Gallery.js
 
-let currentSlide = 0;
+// /**
+//  * Represents a carroussel.
+//  * @class
+//  */
+let data;
 
-function navigate(direction) {
-  const slides = document.querySelectorAll(".slides > div");
-  const totalSlides = slides.length;
+window.onload = async () => {
+  data = await getJson();
 
-  // Calcula o próximo slide
-  currentSlide = (currentSlide + direction + totalSlides) % totalSlides;
+  const gallery = new Gallery(() => console.log(data));
 
-  // Esconde todos os slides
-  slides.forEach((slide) => {
-    slide.style.display = "none";
+  data.forEach((item) => {
+    gallery.addArt(item);
   });
 
-  // Mostra o slide atual
-  slides[currentSlide].style.display = "block";
-}
+  gallery.showSlide();
 
-// Inicializa o carrossel
-window.onload = () => {
-  navigate(0); // Mostra o primeiro slide
+  const about = () => {
+    const a = document.querySelector(".about");
+    const hr = document.createElement("hr");
+    hr.style.height = "5px";
+    hr.style.backgroundColor = "#01372b";
+    hr.style.borderRadius = "20px";
+    a.appendChild(hr);
+  };
+
+  const projects = () => {
+    const p = document.querySelector(".projects");
+    const hr = document.createElement("hr");
+    hr.style.height = "5px";
+    hr.style.backgroundColor = "#01372b";
+    hr.style.borderRadius = "20px";
+    p.appendChild(hr);
+  };
+
+  const contact = () => {
+    const c = document.querySelector(".contact");
+    const hr = document.createElement("hr");
+    hr.style.height = "5px";
+    hr.style.backgroundColor = "#01372b";
+    hr.style.borderRadius = "20px";
+    c.appendChild(hr);
+  };
+
+  document.querySelector(".about").onmouseover = () => {
+    about();
+  };
+
+  document.querySelector(".about").onmouseout = () => {
+    const hr = document.querySelector(".about hr");
+    if (hr) hr.remove();
+  };
+
+  document.querySelector(".projects").onmouseover = () => {
+    projects();
+  };
+
+  document.querySelector(".projects").onmouseout = () => {
+    const hr = document.querySelector(".projects hr");
+    if (hr) hr.remove();
+  };
+
+  document.querySelector(".contact").onmouseover = () => {
+    contact();
+  };
+
+  document.querySelector(".contact").onmouseout = () => {
+    const hr = document.querySelector(".contact hr");
+    if (hr) hr.remove();
+  };
+};
+
+/**
+ * Fetches and returns JSON data from "data.json".
+ * @returns {Promise<Object>} A promise that resolves to the JSON data.
+ */
+const getJson = async () => {
+  const request = await fetch("data.json");
+  const result = await request.json();
+  return result;
 };
