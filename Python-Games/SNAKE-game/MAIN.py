e5eafaa6256487cfb201c4d122bbb72436348a64
Afonso Caboz <afonso.caboz@gmail.com>
2024-03-25 01:03:36 +0000

    minor changes

--- Python-Games/SNAKE-game/MAIN.py
+++ Python-Games/SNAKE-game/MAIN.py
@@ -23,30 +23,32 @@ screen.onkey(snake.right, "Right")
 game_is_on = True
 while game_is_on:
     screen.update()
-    time.sleep(0.1)
+    time.sleep(0.15)
     snake.move()
 
-    #Detect collision with food.
+    # Detect collision with food.
     if snake.head.distance(food) < 15:
         food.refresh()
         snake.extend()
         scoreboard.increase_score()
 
-    #Detect collision with wall.
-    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+    # Detect collision with wall.
+    if (
+        snake.head.xcor() > 280
+        or snake.head.xcor() < -280
+        or snake.head.ycor() > 280
+        or snake.head.ycor() < -280
+    ):
         game_is_on = False
         scoreboard.game_over()
 
-    #Detect collision with tail.
+    # Detect collision with tail.
     for segment in snake.segments:
         if segment == snake.head:
             pass
         elif snake.head.distance(segment) < 10:
             game_is_on = False
             scoreboard.game_over()
-
-
-
 
 
 screen.exitonclick()
